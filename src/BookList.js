f9aaf0529aa7adf0235eb7ae8e556f3ae88c8444
Macbook <frosykat@protonmail.com>
2020-07-03 07:04:14 -0400

    create book components

---
+++ src/BookList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Book from './Book';
+
+class BookList extends Component {
+  /*
+    const { books, shelfTitle } = this.props;
+  */
+  render () {
+    return (
+      <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <li>
+                <Book 
+                  title={"To Kill a Mockingbird"} 
+                  author={"Harper Lee"} 
+                  bookImage={"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"}
+                />
+              </li>
+              <li>
+                <Book 
+                  title={"Ender's Game"} 
+                  author={"Orson Scott Card"} 
+                  bookImage={"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"}
+                />
+              </li>
+            </ol>
+          </div>
+        </div>
+    );
+  }
+}
